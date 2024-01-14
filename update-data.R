ecc09beba26292b2c5fbd59d54fb5081d26b4237
Bustami <ismaelgomezs@gmail.com>
2024-01-14 16:54:56 -0500

    adding chile dts stats to the workflow

--- update-data.R
+++ update-data.R
@@ -3,7 +3,10 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 library(readr)
+library(janitor)
 
+
+# chile games
 path = 'https://www.partidosdelaroja.com/1970/01/partidos-clase-a.html'
 web = read_html(path)
 
@@ -23,3 +26,24 @@ games_ok = games %>%
           filter(!is.na(goals_home))
 
 write_csv(games_ok, "chile_games.csv")
+
+
+# chile dts
+path_dt = "https://www.partidosdelaroja.com/1970/01/entrenadores.html"
+web_dt = read_html(path_dt)
+tables_dt = html_table(web_dt)
+
+data_dt = tables_dt[2] %>% as.data.frame()
+names(data_dt) = data_dt[1, ]
+
+numeric_columns = c("pj", "pg", "pe", "pp", "gf", "gc", "dif")
+
+dt_stats = data_dt %>%
+          clean_names() %>% 
+          rename("dt" = "entrenador") %>% 
+          filter(!dt %in% c("Entrenador", "Total", "Sin entrenador")) %>% 
+          mutate(across(all_of(numeric_columns), ~as.numeric(.x))) %>% 
+          mutate(desde = dmy(desde),
+                 hasta = dmy(hasta)) 
+
+write_csv(dt_stats, "chile_dts_stats.csv")
